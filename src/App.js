0382eb848195cd55867d7a7eb50120e80e87a624
Brian Donnoe <dunow33@hotmail.com>
2020-02-29 04:43:17 -0500

    starting monster card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      string: 'Hello Brian'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.string}</p>
+        <button onClick={() => this.setState({ string: 'dan' })}>Change text</button>
+      </div>
+    );
+  }
 }
 
 export default App;
